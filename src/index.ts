82bb53610af3be53a0a1099f5312b69d7fb63785
AnSeonJeong <asj081@naver.com>
2023-06-13 19:39:33 +0900

    feat : db연동

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
+dotenv.config(); // .env 파일의 환경 변수를 로드
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+});
+
+connection.connect(); // 연결
+
+const sql = "SELECT * FROM MEMBER";
+connection.query(sql, (error, rows, fields) => {
+  if (error) throw error;
+  console.log("Member info is ", rows);
+});
+
+connection.end();
