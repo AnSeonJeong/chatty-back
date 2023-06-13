70f5dce870bb475ee699e43735fd46cbf485883b
AnSeonJeong <asj081@naver.com>
2023-06-13 20:01:00 +0900

    feat : db 설정 파일 분리

---
+++ src/config/db_config.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config(); // .env 파일의 환경 변수를 로드
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+
+const db_config = {
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+};
+
+export default db_config;
