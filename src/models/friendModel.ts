252c154f002654a29e5c34a8108c25a8c33e4c85
AnSeonJeong <asj081@naver.com>
2023-06-15 22:53:14 +0900

    feat : friendModel 생성

---
+++ src/models/friendModel.ts
@@ -0,0 +1,28 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  ForeignKey,
+} from "sequelize-typescript";
+import { User } from "../models/userModel";
+
+@Table({ tableName: "friends" })
+export class Friend extends Model {
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  user_id!: number;
+
+  @Column({ type: DataType.INTEGER })
+  friend_id!: number;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: 0,
+  })
+  status!: boolean;
+}
