7539721ce9be7baf2d88bab0a636e0b157e94e77
AnSeonJeong <asj081@naver.com>
2023-06-15 22:47:47 +0900

    test : sequelize 연결 테스트

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,23 @@
 import express from "express";
 import mysql from "mysql2";
 import db_config from "./config/db_config";
+import sequelize from "./db/connection";
 const cors = require("cors");
-const connection = mysql.createConnection(db_config);
+// const connection = mysql.createConnection(db_config);
 const app = express();
 
 // 설정
 app.use(cors()); // 모든 도메인에서 요청과 응답을 받을 수 있도록 허용
 app.set("port", process.env.PORT || 3000);
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
-app.get("/users", (req, res) => {
-  const sql = "SELECT * FROM MEMBER";
-  connection.query(sql, (error, rows, fields) => {
-    if (error) throw error;
-    console.log("Member info is ", rows); // 콘솔에 해당 값이 출력되면 연결 성공
-    res.send(rows);
-  });
-});
-
-app.listen(app.get("port"), () => {
+app.listen(app.get("port"), async () => {
   console.log("Express server listening on port " + app.get("port"));
+
+  // 연결 테스트
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 });
