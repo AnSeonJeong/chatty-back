72b8a1fcf4667622f8ca0ccdf9bae92bef37efe5
AnSeonJeong <asj081@naver.com>
2023-06-20 18:12:59 +0900

    docs : wrapAsync에 주석 추가

--- src/utils/wrapAsync.ts
+++ src/utils/wrapAsync.ts
@@ -8,6 +8,7 @@ type AsyncRequestHandler = (
 
 export const wrapAsync = (fn: AsyncRequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
+    // 모든 오류를 .catch() 처리 후 next() 미들웨어에 전달
     fn(req, res, next).catch(next);
   };
 };
