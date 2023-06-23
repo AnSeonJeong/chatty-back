84704d69663dea1394283e97c365067cbcf38214
AnSeonJeong <asj081@naver.com>
2023-06-23 17:07:45 +0900

    feat : 로그인 routes 설정

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -9,6 +9,8 @@ const userController = new UserController();
 router.post("/regi", wrapAsync(userController.addUser));
 
 // 로그인
+router.get("/login/social", wrapAsync(userController.socialConfig));
+router.get("/redirect", wrapAsync(userController.socialLogin));
 
 // 로그아웃
 
