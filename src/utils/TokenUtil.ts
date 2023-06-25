9e8a3ebb8cab155c55bd733a86897aa98eab40a6
AnSeonJeong <asj081@naver.com>
2023-06-25 18:24:39 +0900

    feat : 클래스명 변경 & 토큰 검증 로직 추가

--- src/utils/TokenUtil.ts
+++ src/utils/TokenUtil.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export class GenerateJwtUtil {
+export class TokenUtil {
   private id: number;
   private nickname: string;
 
@@ -9,14 +9,28 @@ export class GenerateJwtUtil {
     this.nickname = nickname;
   }
 
+  // 토큰 생성
   public generateToken(secretKey: string, expiresIn: string) {
     const payload = {
       id: this.id,
       nickname: this.nickname,
-      //   exp: Math.floor(Date.now() / 1000) + expiresIn,
     };
 
     const token = jwt.sign(payload, secretKey, { expiresIn: expiresIn });
     return token;
+  }
+
+  // 토큰 검증
+  public validateToken(token: string, secretKey: string) {
+    let isValid: boolean;
+    try {
+      // 토큰이 유효한 경우
+      jwt.verify(token, secretKey);
+      isValid = true;
+    } catch (error) {
+      // 토큰이 유효하지 않은 경우
+      isValid = false;
+    }
+    return isValid;
   }
 }
