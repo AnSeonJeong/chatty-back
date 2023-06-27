cb60b985bb39ec552dc4cd5db4528950b317311b
AnSeonJeong <asj081@naver.com>
2023-06-27 20:59:53 +0900

    feat : 로그인 추가 &  form-data를 다루기 위해 multer 추가

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,14 +1,21 @@
 import { Router } from "express";
 import { UserController } from "../controllers/UserController";
 import { wrapAsync } from "../utils/wrapAsyncUtil";
+import multer from "multer";
 
 const router = Router();
+const upload = multer();
 const userController = new UserController();
 
 // 회원가입
 router.post("/regi", wrapAsync(userController.addUser));
 
 // 로그인
+router.post(
+  "/login",
+  upload.array("login_info"),
+  wrapAsync(userController.login)
+);
 router.get("/login/social", wrapAsync(userController.socialConnection));
 router.get("/redirect", wrapAsync(userController.socialLogin));
 
