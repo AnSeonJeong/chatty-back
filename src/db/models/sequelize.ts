e5fa77636194ed1494f2571e2b1970d749475f1c
AnSeonJeong <asj081@naver.com>
2023-06-29 01:19:30 +0900

    fix : sequelize 설정 수정 - model 추가

--- src/db/models/sequelize.ts
+++ src/db/models/sequelize.ts
@@ -1,8 +1,12 @@
 import { Sequelize } from "sequelize-typescript";
 import db_config from "../../config/db_config";
 
-const sequelize = new Sequelize(db_config);
-
-sequelize.addModels([__dirname + "/models"]);
+const sequelize = new Sequelize({
+  ...db_config,
+  models: [__dirname + "/*Model.ts"],
+  modelMatch: (filename, member) => {
+    return filename.substring(0, filename.indexOf("Model")) === member;
+  },
+});
 
 export default sequelize;
