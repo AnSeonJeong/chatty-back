0c04af240ce91ac3cc753db9bd1b9a42580505ee
AnSeonJeong <asj081@naver.com>
2023-06-28 21:37:34 +0900

    fix : 파일 업로드 설정 수정

--- src/config/upload_config.ts
+++ src/config/upload_config.ts
@@ -8,17 +8,17 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // 동적으로 폴더 경로 생성
     const subfolder = determineSubfolder(file);
-    const uploadPath = path.join("uploads", subfolder);
+    const uploadPath = path.join("src/uploads", subfolder);
     cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname + "-" + Date.now());
+    cb(null, `${Date.now()}.${file.mimetype.split("/")[1]}`);
   },
 });
 // 파일 저장 폴더 경로를 지정
 const determineSubfolder = (file: MulterFile) => {
   if (file.fieldname === "profile") {
-    return "user/profiles";
+    return "user-profiles";
   } else if (file.fieldname === "chatImage") {
     return "chat/images";
   } else if (file.fieldname === "chatDocument") {
