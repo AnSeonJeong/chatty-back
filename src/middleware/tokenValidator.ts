9fd0e1528276124d943e32b9aabd43f5dbd8534e
AnSeonJeong <asj081@naver.com>
2023-06-29 18:01:24 +0900

    fix :  TokenUtil을 토큰 생성 유틸리티와 토큰 검증 미들웨어로 분리

---
+++ src/middleware/tokenValidator.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { CustomError } from "../errors/CustomError";
+import { HttpCode } from "../errors/HttpCode";
+
+export const tokenValidator = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authorizationHeader = req.headers.authorization;
+
+  if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
+    const token = authorizationHeader.slice(7); // "Bearer " 부분을 제외한 나머지 문자열을 추출
+
+    try {
+      const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+      req.decoded = decodedToken; // 토큰의 복호화된 데이터를 요청 객체의 속성에 저장
+      next(); // 다음 미들웨어로 이동
+    } catch (error) {
+      console.log(error);
+      throw new CustomError("Invalid token", HttpCode.UNAUTHORIZED);
+    }
+  } else {
+    throw new CustomError("Invalid token", HttpCode.UNAUTHORIZED);
+  }
+};
--- src/utils/GenerateTokenUtil.ts
+++ src/utils/GenerateTokenUtil.ts
@@ -1,13 +1,12 @@
 import jwt from "jsonwebtoken";
 
-export class TokenUtil {
+export class GenerateTokenUtil {
   private secretKey: string;
 
   constructor(secretKey: string) {
     this.secretKey = secretKey;
   }
 
-  // 토큰 생성
   public generateToken(id: number, nickname: string, expiresIn: string) {
     const payload = {
       id: id,
@@ -16,19 +15,5 @@ export class TokenUtil {
 
     const token = jwt.sign(payload, this.secretKey, { expiresIn: expiresIn });
     return token;
-  }
-
-  // 토큰 검증
-  public validateToken(token: string) {
-    let isValid: boolean;
-    try {
-      // 토큰이 유효한 경우
-      jwt.verify(token, this.secretKey);
-      isValid = true;
-    } catch (error) {
-      // 토큰이 유효하지 않은 경우
-      isValid = false;
-    }
-    return isValid;
   }
 }
