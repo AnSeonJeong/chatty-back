d74dee70cd71926f53044b53fe51de61c5388e5d
AnSeonJeong <asj081@naver.com>
2023-07-07 01:51:59 +0900

    feat : 채팅 메시지 저장 - ChatRouter 작성

--- src/routes/ChatRouter.ts
+++ src/routes/ChatRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { wrapAsync } from "../utils/wrapAsyncUtil";
 import { ChatController } from "../controllers/ChatController";
+import upload from "../config/upload_config";
 
 const router = Router();
 const chatController = new ChatController();
@@ -8,5 +9,10 @@ const chatController = new ChatController();
 router.get("/chats", wrapAsync(chatController.getChatrooms));
 
 router.get("/chats/:room_id", wrapAsync(chatController.getChatList));
+router.post(
+  "/chats/:room_id/message",
+  upload.single("send_message"),
+  wrapAsync(chatController.saveChatting)
+);
 
 export default router;
