f1fc1a333c36b08131b69c212d55c2fad42a207f
AnSeonJeong <asj081@naver.com>
2023-07-28 00:14:12 +0900

    feat : notification 테이블 생성 migration

---
+++ src/db/migrations/20230727075608-create_notification_table.ts.ts
@@ -0,0 +1,38 @@
+"use strict";
+
+import { QueryInterface, DataTypes } from "sequelize";
+
+module.exports = {
+  up: async (queryInterface: QueryInterface) => {
+    await queryInterface.createTable("notification", {
+      room_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: "rooms",
+          },
+          key: "id",
+        },
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: "users",
+          },
+          key: "id",
+        },
+      },
+      count: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.dropTable("notification");
+  },
+};
