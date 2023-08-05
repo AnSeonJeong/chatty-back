2ada8b4b54f874109beaf46b4742c0511dd30c30
AnSeonJeong <asj081@naver.com>
2023-08-05 19:09:53 +0900

    refactor : API 리팩토링 - 친구 관련 엔드포인트 수정

--- src/routes/FriendRouter.ts
+++ src/routes/FriendRouter.ts
@@ -9,14 +9,17 @@ const friendController = new FriendController();
 router.get("/friends", wrapAsync(friendController.getAllFriends));
 
 // 친구 요청, 삭제
-router.post("/friends/add/:friend_id", wrapAsync(friendController.addFriend));
+router.post(
+  "/friends/request/:friend_id",
+  wrapAsync(friendController.addFriend)
+);
 router.post(
   "/friends/remove/:friend_id",
   wrapAsync(friendController.removeFriend)
 );
 
 // 친구 요청 목록 불러오기
-router.get("/profile", wrapAsync(friendController.getAllFriends));
+router.get("/friends/requests", wrapAsync(friendController.getAllFriends));
 
 // 친구 요청 수락, 거절
 router.get(
