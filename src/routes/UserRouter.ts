663dc646b1ef148b1e734d911764339e450d4364
AnSeonJeong <asj081@naver.com>
2023-11-27 20:43:18 +0900

    refactor : API 리팩토링 - 회원 관련 엔드포인트 및 메서드 구조 개선

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -9,15 +9,9 @@ const userController = new UserController();
 
 // 회원가입
 router.post(
-  "/regi",
+  "/users",
   upload.single("regi_info"),
   wrapAsync(userController.addUser)
-);
-
-router.post(
-  "/regi/upload",
-  upload.single("profile"),
-  wrapAsync(userController.saveProfileImage)
 );
 
 // 로그인
@@ -32,27 +26,24 @@ router.get("/redirect", wrapAsync(userController.socialLogin));
 // 발급받은 토큰 검증
 router.use(tokenValidator);
 // 사용자 정보 불러오기
-router.get("/main", wrapAsync(userController.getUser));
 router.get("/users/:user_id", wrapAsync(userController.getUser));
-router.get("/users/search/:nickname", wrapAsync(userController.searchUser));
+router.get("/users", wrapAsync(userController.searchUser));
 
 // 회원 정보 수정
-router.post(
-  "/users/profile-update",
+router.patch(
+  "/users/:user_id",
   upload.single("update_info"),
   wrapAsync(userController.updateUserInfo)
 );
 
 // 회원 프로필 이미지 수정
 router.post(
-  "/users/profile-update/image",
+  "/users/:user_id/profile-images",
   upload.single("profile"),
   wrapAsync(userController.updateProfileImage)
 );
 
 // 회원 탈퇴
-router.delete("/users/delete/:user_id", wrapAsync(userController.deleteUser));
-
-// 로그아웃
+router.delete("/users/:user_id", wrapAsync(userController.deleteUser));
 
 export default router;
